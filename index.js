1108d3339bcf7939845e283d86608d9fade709d3
AidanLincoln <aidantlincoln@gmail.com>
2020-03-09 17:10:59 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function wakeDog(dogName, dogBreed) {
+    const str = `Wake ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+function leashDog(dogName, dogBreed) {
+    const str = `Leash ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+function walkToPark(dogName, dogBreed) {
+    const str = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+function throwFrisbee(dogName, dogBreed) {
+    const str = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+function walkHome(dogName, dogBreed) {
+    const str = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+function unleashDog(dogName, dogBreed) {
+    const str = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str
+}
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    const results = []
+    for(let i = 0; i < routine.length; i++) {
+        results.push(routine[i](dogName, dogBreed))
+    }
+    return results
+}
