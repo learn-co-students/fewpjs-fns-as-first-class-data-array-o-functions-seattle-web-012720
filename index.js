942883b39c8261d5fa4d093f17989cb1a825fc38
javila35 <josepharthuravila@gmail.com>
2020-03-11 16:35:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function wakeDog(name, breed) {
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${name} the ${breed}`;
 };
+
+function leashDog(name, breed) {
+    return `Leash ${name} the ${breed}`
+};
+
+function walkToPark(name, breed) {
+    return `Walk to the park with ${name} the ${breed}`
+};
+
+function throwFrisbee(name, breed) {
+    return `Throw the frisbee for ${name} the ${breed}`
+};
+
+function walkHome(name, breed) {
+    return `Walk home with ${name} the ${breed}`
+};
+
+function unleashDog(name, breed) {
+    return `Unleash ${name} the ${breed}`
+};
+
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
+
+function exerciseDog(dog_name, dog_breed) {
+    return routine.map(fn => fn(dog_name, dog_breed))
+      }
