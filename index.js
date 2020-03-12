22d8a63e0a18b21ff3c6915013d134b09c8ede7d
github username <github email address>
2020-03-12 13:10:15 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function wakeDog(dogName, dogBreed){
+    return `Wake ${dogName} the ${dogBreed}`
+};
+function leashDog(dogName, dogBreed) {
+    return `Leash ${dogName} the ${dogBreed}`
+};
+function walkToPark(dogName, dogBreed) {
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+};
+function throwFrisbee(dogName, dogBreed) {
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+};
+function walkHome(dogName, dogBreed) {
+    return `Walk home with ${dogName} the ${dogBreed}`
+};
+function unleashDog(dogName, dogBreed) {
+    return `Unleash ${dogName} the ${dogBreed}`
+};
+
+const routine = [
+    wakeDog(dogName, dogBreed), 
+    leashDog(dogName, dogBreed),
+    walkToPark(dogName, dogBreed),
+    throwFrisbee(dogName, dogBreed),
+    walkHome(dogName, dogBreed),
+    unleashDog(dogName, dogBreed)
+]
+
+function exerciseDog(dogName, dogBreed) {
+     return routine.map(function(method) {
+        method(dogName, dogBreed);
+    })
+};
